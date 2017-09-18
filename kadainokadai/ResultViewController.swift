bede730149d1bb0219414954fa6010c08d5f3d82
akiko nakama <akikonakama@akiko-no-MacBook-Air.local>
2017-09-18 18:46:10 +0900

    課題の追加

---
+++ kadainokadai/ResultViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ResultViewController.swift
+//  kadainokadai
+//
+//  Created by akiko nakama on 2017/09/18.
+//  Copyright © 2017年 akiko nakama. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    var label:String = "name"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+               label.text = "こんにちは \(name) さん"
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
